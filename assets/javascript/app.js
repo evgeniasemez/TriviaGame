8d2b7a27b78d697f0e2a5e28467b46c67f7a88df
Evgenia Semez <esemez@indeed.com>
2019-03-24 19:49:12 -0700

    adding an amazing animated title

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,50 @@
 $(document).ready(function () {
+    var message = "Trivia Game";
+    var msgCount = 0;
+    var blinkCount = 0;
+    var blinkFlg = 0;
+    var timer1, timer2;
+    var messageLabel = document.getElementById("messageLabel");
+
+
+    function textFunc() {
+        console.log("We made it into textFunc()")
+        messageLabel.innerHTML = message.substring(0, msgCount);
+
+        if (msgCount == message.length) {
+            // Stop Timer
+            clearInterval(timer1);
+
+            // Start blinking animation!
+            timer2 = setInterval(blinkFunc, 200);
+
+        } else {
+            msgCount++;
+        }
+    }
+
+    function blinkFunc() {
+
+        // Blink 5 times
+        if (blinkCount < 6) {
+            if (blinkFlg == 0) {
+                messageLabel.innerHTML = message;
+                blinkFlg = 1;
+                blinkCount++;
+            } else {
+                messageLabel.innerHTML = "";
+                blinkFlg = 0;
+            }
+        } else {
+            // Stop Timer
+            clearInterval(timer2);
+        }
+    }
+
+
+    timer1 = setInterval(textFunc, 150); // Every 150 milliseconds
+
+
 
     $("#startbutton").on("click", function () {
         $("#startbutton").html("");
@@ -11,7 +57,7 @@ $(document).ready(function () {
 
     $("#remaining_timer").text("30");
     function runtimer() {
-        timeRemaining =30;
+        timeRemaining = 30;
         // clearInterval(intervalId);
         intervalId = setInterval(decrement, 1000);
     }
@@ -45,6 +91,10 @@ $(document).ready(function () {
 
 
 
+
+
+
+    
 
     // var correctAnswers = 0;
     // var incorrectAnswers = 0;
