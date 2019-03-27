f2f8a76cef073c989d02708b66ad235f641c8e2b
Evgenia Semez <esemez@indeed.com>
2019-03-26 17:51:12 -0700

    setting the result card and score count

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -44,20 +44,37 @@ $(document).ready(function () {
 
     timer1 = setInterval(textFunc, 150); // Every 150 milliseconds
 
-
+    var correctAnswer = 0;
+    var wrongAnswer = 0;
+    var unansweredAnswer = 0;
     // setting the button click and navigation to the next block 
     $("#startbutton").on("click", function () {
-        $("#startbutton").html("");
+        $("#startbutton").css({ display: "none" });
         $(".secondcard").css({ display: "block" });
-        runtimer();
+
+        nextstep = 0;
+        $("#question_itself").html("<h4>" + arrayOfQuestions[nextstep].Question + "</h4>");
+        $("#first_option").html("<h4>" + arrayOfQuestions[nextstep].Options[0] + "</h4>");
+        $("#second_option").html("<h4>" + arrayOfQuestions[nextstep].Options[1] + "</h4>");
+        $("#third_option").html("<h4>" + arrayOfQuestions[nextstep].Options[2] + "</h4>");
+        $("#fourth_option").html("<h4>" + arrayOfQuestions[nextstep].Options[3] + "</h4>");
+        $(".question").css({ display: "block" });
+        $(".wrongAnswerTitle").css({ display: "none" });
+        $(".correctAnswerTitle").css({ display: "none" });
+        $(".timeOutCard").css({ display: "none" });
+        reset();
+        resetNumbers();
     });
 
     $("#third_option").on("click", function () {
+
         $(".question").css({ display: "none" });
         if (arrayOfQuestions[nextstep].rightOption === 2) {
             $(".correctAnswerTitle").css({ display: "block" });
+            correctAnswer++;
         }
         else {
+            wrongAnswer++;
             $(".wrongAnswerTitle").css({ display: "block" });
         }
         correctOption();
@@ -68,8 +85,10 @@ $(document).ready(function () {
         $(".question").css({ display: "none" });
         if (arrayOfQuestions[nextstep].rightOption === 0) {
             $(".correctAnswerTitle").css({ display: "block" });
+            correctAnswer++;
         }
         else {
+            wrongAnswer++;
             $(".wrongAnswerTitle").css({ display: "block" });
         }
         correctOption();
@@ -80,8 +99,10 @@ $(document).ready(function () {
         $(".question").css({ display: "none" });
         if (arrayOfQuestions[nextstep].rightOption === 1) {
             $(".correctAnswerTitle").css({ display: "block" });
+            correctAnswer++;
         }
         else {
+            wrongAnswer++;
             $(".wrongAnswerTitle").css({ display: "block" });
         }
         correctOption();
@@ -89,16 +110,24 @@ $(document).ready(function () {
         stop();
     });
     $("#fourth_option").on("click", function () {
+
         $(".question").css({ display: "none" });
         if (arrayOfQuestions[nextstep].rightOption === 3) {
             $(".correctAnswerTitle").css({ display: "block" });
+            correctAnswer++;
         }
         else {
             $(".wrongAnswerTitle").css({ display: "block" });
+            wrongAnswer++;
         }
         correctOption();
         setTimeout(nextQuestion, 5000);
         stop();
+    });
+    $("#startOver").on("click", function () {
+        $("#startbutton").css({ display: "block" });
+        $(".secondcard").css({ display: "none" });
+        $(".resultCard").css({ display: "none" });
     });
 
 
@@ -129,6 +158,7 @@ $(document).ready(function () {
 
             setTimeout(nextQuestion, 5000);
             //  ...run the stop function.
+            unansweredAnswer++;
             correctOption();
             stop();
 
@@ -157,18 +187,29 @@ $(document).ready(function () {
 
     function nextQuestion() {
         nextstep++;
+        if (nextstep > 3) {
+            $(".correctAnswersCount").html(correctAnswer);
+            $(".wrongAnswersCount").html(wrongAnswer);
+            $(".unansweredAnswersCount").html(unansweredAnswer);
 
-        $("#question_itself").html("<h4>" + arrayOfQuestions[nextstep].Question + "</h4>");
-        $("#first_option").html("<h4>" + arrayOfQuestions[nextstep].Options[0] + "</h4>");
-        $("#second_option").html("<h4>" + arrayOfQuestions[nextstep].Options[1] + "</h4>");
-        $("#third_option").html("<h4>" + arrayOfQuestions[nextstep].Options[2] + "</h4>");
-        $("#fourth_option").html("<h4>" + arrayOfQuestions[nextstep].Options[3] + "</h4>");
-        $(".question").css({ display: "block" });
-        $(".wrongAnswerTitle").css({ display: "none" });
-        $(".correctAnswerTitle").css({ display: "none" });
-        $(".timeOutCard").css({ display: "none" });
-        $(".resultCard").css({ display: "block" });
-        reset();
+            $(".resultCard").css({ display: "block" });
+            $(".correctAnswerTitle").css({ display: "none" });
+            $(".wrongAnswerTitle").css({ display: "none" });
+            $(".timeOutCard").css({ display: "none" });
+        }
+        else {
+
+            $("#question_itself").html("<h4>" + arrayOfQuestions[nextstep].Question + "</h4>");
+            $("#first_option").html("<h4>" + arrayOfQuestions[nextstep].Options[0] + "</h4>");
+            $("#second_option").html("<h4>" + arrayOfQuestions[nextstep].Options[1] + "</h4>");
+            $("#third_option").html("<h4>" + arrayOfQuestions[nextstep].Options[2] + "</h4>");
+            $("#fourth_option").html("<h4>" + arrayOfQuestions[nextstep].Options[3] + "</h4>");
+            $(".question").css({ display: "block" });
+            $(".wrongAnswerTitle").css({ display: "none" });
+            $(".correctAnswerTitle").css({ display: "none" });
+            $(".timeOutCard").css({ display: "none" });
+            reset();
+        }
     }
 
     var arrayOfQuestions = [{
@@ -190,5 +231,12 @@ $(document).ready(function () {
     }
     ];
     var nextstep = 0;
+
+    function resetNumbers() {
+        correctAnswer = 0;
+        wrongAnswer = 0;
+        unansweredAnswer = 0;
+    }
+
 
 });
